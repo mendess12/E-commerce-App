20e76a974924af36ecb72c5e15a75545cda4901f
Yusuf Mendeş <yusufmendes123@gmail.com>
2023-09-22 00:44:23 +0300

    Add bag screen design

---
+++ app/src/main/java/com/yusufmendes/sisterslabgraduationproject/view/bag/BagFragment.kt
@@ -0,0 +1,13 @@
+package com.yusufmendes.sisterslabgraduationproject.view.bag
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.yusufmendes.sisterslabgraduationproject.R
+
+class BagFragment : Fragment(R.layout.fragment_bag) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
