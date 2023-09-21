d5ae50749486dc4ec8e1d483a53334651c0aae4c
Yusuf Mendeş <yusufmendes123@gmail.com>
2023-09-22 01:42:58 +0300

    Add bottom navigation view

--- app/src/main/java/com/yusufmendes/sisterslabgraduationproject/MainActivity.kt
+++ app/src/main/java/com/yusufmendes/sisterslabgraduationproject/MainActivity.kt
@@ -2,10 +2,25 @@ package com.yusufmendes.sisterslabgraduationproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.yusufmendes.sisterslabgraduationproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        NavigationUI.setupWithNavController(
+            binding.bottomNavigationView,
+            navHostFragment.navController
+        )
     }
 }
