9cd7f817f6e4a3dcb95443f9f47d7acc1b30a8a4
Yusuf Mendeş <yusufmendes123@gmail.com>
2023-09-22 00:45:24 +0300

    Add home screen design

---
+++ app/src/main/java/com/yusufmendes/sisterslabgraduationproject/view/home/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.yusufmendes.sisterslabgraduationproject.view.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.yusufmendes.sisterslabgraduationproject.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
