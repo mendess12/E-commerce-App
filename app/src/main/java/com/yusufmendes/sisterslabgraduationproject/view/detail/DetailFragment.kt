d2c7a800f9e0d4ea2eefae2093ab6f7cc1068b4b
Yusuf Mendeş <yusufmendes123@gmail.com>
2023-09-22 01:13:19 +0300

    Add detail screen design

---
+++ app/src/main/java/com/yusufmendes/sisterslabgraduationproject/view/detail/DetailFragment.kt
@@ -0,0 +1,14 @@
+package com.yusufmendes.sisterslabgraduationproject.view.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.yusufmendes.sisterslabgraduationproject.R
+
+
+class DetailFragment : Fragment(R.layout.fragment_detail) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
