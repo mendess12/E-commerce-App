3e4fe5008d1b8fe6e9850429613d08d8d1f50a94
Yusuf Mendeş <yusufmendes123@gmail.com>
2023-09-22 15:10:57 +0300

    Add repository

---
+++ app/src/main/java/com/yusufmendes/sisterslabgraduationproject/repository/ProductRepository.kt
@@ -0,0 +1,11 @@
+package com.yusufmendes.sisterslabgraduationproject.repository
+
+import com.yusufmendes.sisterslabgraduationproject.model.Product
+import com.yusufmendes.sisterslabgraduationproject.services.ProductAPI
+import retrofit2.Response
+import javax.inject.Inject
+
+class ProductRepository @Inject constructor(private val productAPI: ProductAPI) {
+
+    suspend fun getProducts(): Response<Product> = productAPI.getProductData()
+}
