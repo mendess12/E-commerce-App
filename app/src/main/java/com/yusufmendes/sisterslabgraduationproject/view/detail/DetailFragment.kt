be0d3a3564a73e4aca97e008b46c0bc50bb4cdaa
Yusuf Mendeş <yusufmendes123@gmail.com>
2023-09-22 18:47:01 +0300

    Add detail and view pager 2

--- app/src/main/java/com/yusufmendes/sisterslabgraduationproject/view/detail/DetailFragment.kt
+++ app/src/main/java/com/yusufmendes/sisterslabgraduationproject/view/detail/DetailFragment.kt
@@ -1,22 +1,89 @@
 package com.yusufmendes.sisterslabgraduationproject.view.detail
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
 import com.yusufmendes.sisterslabgraduationproject.R
+import com.yusufmendes.sisterslabgraduationproject.adapter.ViewPagerAdapter
 import com.yusufmendes.sisterslabgraduationproject.databinding.FragmentDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.math.abs
 
 @AndroidEntryPoint
 class DetailFragment : Fragment(R.layout.fragment_detail) {
 
     private lateinit var binding: FragmentDetailBinding
-
+    private val args: DetailFragmentArgs by navArgs()
+    private lateinit var viewPagerAdapter: ViewPagerAdapter
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var handler: Handler
+    private lateinit var imageList: ArrayList<String>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentDetailBinding.bind(view)
 
+        val cart = args.product
+        with(binding) {
+            detailScreenTitleTv.text = cart.title
+            detailScreenDescriptionTv.text = cart.description
+            detailScreenRateTv.text = cart.rate.toString()
+            detailScreenPriceTv.text = cart.price.toString()
+        }
+
+        binding.detailScreenToolbar.backToolBar.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        init()
+        setUpTransformer()
+
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                handler.removeCallbacks(runnable)
+            }
+        })
+    }
+
+    private val runnable = Runnable {
+        viewPager2.currentItem = viewPager2.currentItem + 1
+    }
+
+    private fun setUpTransformer() {
+        val transformer = CompositePageTransformer()
+        transformer.addTransformer(MarginPageTransformer(50))
+        transformer.addTransformer { page, position ->
+            val r = 1 - abs(position)
+            page.scaleY = 0.85f + r * 0.14f
+        }
+        viewPager2.setPageTransformer(transformer)
+    }
+
+    private fun init() {
+        viewPager2 = binding.detailScreenViewPager2
+        handler = Handler(Looper.myLooper()!!)
+
+        imageList = ArrayList()
+        imageList.add(args.product.imageOne)
+        imageList.add(args.product.imageThree)
+        imageList.add(args.product.imageTwo)
+        viewPagerAdapter = ViewPagerAdapter(imageList, viewPager2)
+
+
+        viewPager2.adapter = viewPagerAdapter
+        viewPager2.offscreenPageLimit = 3
+        viewPager2.clipToPadding = false
+        viewPager2.clipChildren = false
+        viewPager2.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
 
     }
 }
