2353acbb7612253b15b5c7ef6ac5bb7765dad9ea
Yusuf Mendeş <yusufmendes123@gmail.com>
2023-09-23 22:49:32 +0300

    Add discount line and sale price

--- app/src/main/java/com/yusufmendes/sisterslabgraduationproject/adapter/BagProductAdapter.kt
+++ app/src/main/java/com/yusufmendes/sisterslabgraduationproject/adapter/BagProductAdapter.kt
@@ -1,8 +1,10 @@
 package com.yusufmendes.sisterslabgraduationproject.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.yusufmendes.sisterslabgraduationproject.R
 import com.yusufmendes.sisterslabgraduationproject.databinding.BagItemBinding
 import com.yusufmendes.sisterslabgraduationproject.model.ProductX
 import com.yusufmendes.sisterslabgraduationproject.util.downloadFromUrl
@@ -11,12 +13,24 @@ class BagProductAdapter : RecyclerView.Adapter<BagProductAdapter.BagViewHolder>(
 
     private val bagProductList = ArrayList<ProductX>()
 
-    inner class BagViewHolder(val binding: BagItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class BagViewHolder(private val binding: BagItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
         fun bind(bagProduct: ProductX) {
             with(binding) {
                 bagItemTitleTv.text = bagProduct.title
-                bagItemPriceTv.text = bagProduct.price.toString()
+                bagItemPriceTv.text =
+                    binding.root.context.getString(R.string.price, bagProduct.price.toString())
                 bagItemIv.downloadFromUrl(bagProduct.imageOne)
+                if (bagProduct.saleState) {
+                    bagItemSalePriceTv.visibility = View.VISIBLE
+                    bagItemSalePriceTv.text =
+                        binding.root.context.getString(
+                            R.string.price,
+                            bagProduct.salePrice.toString()
+                        )
+                    bagItemPriceTv.setBackgroundResource(R.drawable.discount_line)
+                }
             }
         }
     }
