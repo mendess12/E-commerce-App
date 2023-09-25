e50a40f27aa433766208888af657f8762c09f634
Yusuf Mendeş <yusufmendes123@gmail.com>
2023-09-25 23:11:07 +0300

    Add category and snackbar design

--- app/src/main/java/com/yusufmendes/sisterslabgraduationproject/util/extensions/Snackbar.kt
+++ app/src/main/java/com/yusufmendes/sisterslabgraduationproject/util/extensions/Snackbar.kt
@@ -1,8 +1,12 @@
 package com.yusufmendes.sisterslabgraduationproject.util.extensions
 
+import android.graphics.Color
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 
 fun View.showSnackbar(message:String) {
-    Snackbar.make(this,message,Snackbar.LENGTH_SHORT).show()
+    val snackBar = Snackbar.make(this, message, Snackbar.LENGTH_LONG)
+    snackBar.setTextColor(Color.WHITE)
+    snackBar.setBackgroundTint(Color.RED)
+    snackBar.show()
 }
